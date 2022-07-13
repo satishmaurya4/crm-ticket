4b5073e8872e5bb3641637abf6575bddcc031e68
satishmaurya4 <mauryasatish4488@gmail.com>
2022-07-13 13:14:13 +0530

    update

--- src/components/TicketRecordsTable.js
+++ src/components/TicketRecordsTable.js
@@ -1,72 +1,68 @@
-import React from 'react'
+import React from "react";
 import MaterialTable from "@material-table/core";
 import { ExportCsv, ExportPdf } from "@material-table/exporters";
 
-
-const TicketRecordsTable = ({ editTicket, ticketRecord, admin, engineer, customer }) => {
+const TicketRecordsTable = ({ editTicket, ticketRecord, admin, engineer }) => {
   return (
-      <div>
-          {/* <h4>{tableTitle}</h4> */}
-  < MaterialTable
-      className={admin ? 'admin-table' : engineer ? 'eng-table' : 'cx-table'}
-            onRowClick={(event, rowData) => editTicket(rowData)}
-            columns={[
-              {
-                title: "Ticket ID",
-                field: "id",
-              },
-              {
-                title: "TITLE",
-                field: "title",
-              },
-              {
-                title: "DESCRIPTION",
-                field: "description",
-              },
-              {
-                title: "REPORTER",
-                field: "reporter",
-              },
-              {
-                title: "PRIORITY",
-                field: "ticketPriority",
-              },
-              {
-                title: "Assignee",
-                field: "assignee",
-              },
+    <div>
+      <MaterialTable
+        className={admin ? "admin-table" : engineer ? "eng-table" : "cx-table"}
+        onRowClick={(event, rowData) => editTicket(rowData)}
+        columns={[
+          {
+            title: "Ticket ID",
+            field: "id",
+          },
+          {
+            title: "TITLE",
+            field: "title",
+          },
+          {
+            title: "DESCRIPTION",
+            field: "description",
+          },
+          {
+            title: "REPORTER",
+            field: "reporter",
+          },
+          {
+            title: "PRIORITY",
+            field: "ticketPriority",
+          },
+          {
+            title: "Assignee",
+            field: "assignee",
+          },
 
-              {
-                title: "Status",
-                field: "status",
-              },
-            ]}
-            options={{
-              exportMenu: [
-                {
-                  label: "Export Pdf",
-                  exportFunc: (cols, datas) =>
-                    ExportPdf(cols, datas, "Ticket Records"),
-                },
-                {
-                  label: "Export Csv",
-                  exportFunc: (cols, datas) =>
-                    ExportCsv(cols, datas, "Ticket Records"),
-                },
-              ],
-             
-              headerStyle: {
-                
-              },
-              rowStyle: {
-                backgroundColor: "#fff",
-              },
-            }}
-            data={ticketRecord}
-            title=""
-          />
+          {
+            title: "Status",
+            field: "status",
+          },
+        ]}
+        options={{
+          exportMenu: [
+            {
+              label: "Export Pdf",
+              exportFunc: (cols, datas) =>
+                ExportPdf(cols, datas, "Ticket Records"),
+            },
+            {
+              label: "Export Csv",
+              exportFunc: (cols, datas) =>
+                ExportCsv(cols, datas, "Ticket Records"),
+            },
+          ],
+
+          headerStyle: {},
+          rowStyle: {
+            backgroundColor: "#fff",
+          },
+        }}
+        data={ticketRecord}
+        title=""
+      />
     </div>
-  )
-}
+  );
+};
 
-export default TicketRecordsTable
+export default TicketRecordsTable;
